49399e07f251ee1312f8192efedc71a3186933d3
PPG-BrianChan <cchan@ppg.com>
2022-03-24 05:12:28 +0000

    Completed include hana and deployment

--- srv/usermanagement-service.js
+++ srv/usermanagement-service.js
@@ -12,23 +12,23 @@ module.exports = async function (srv) {
         return users.run(req.query);
     });
 
-    // srv.before("CREATE", "incidents", async (req) => {
-    //     console.log(`start of hook`);
-    //     console.log(srv.entities);
-    //     const { incidents } = srv.entities;
-    //     const query_get_ticketno = SELECT.one
-    //       .from(incidents)
-    //       .columns("max(ticket_no) as ticketno");
-    //     const result = await cds.run(query_get_ticketno);
-    //     const jsonobj = JSON.parse(JSON.stringify(result));
-    //     var ticketno = `${jsonobj.ticketno} + 1`;
-    //     req.data.ticket_no = JSON.stringify(eval(ticketno));
-    //     req.data.status = `PENDING`;
-    //     req.data.approverid = `K009287`;
-    // })
-    // this.on('CREATE', 'incident', async (req, msg) => {
-    //     createwf(msg.data);
-    // });
+    srv.before("CREATE", "incidents", async (req) => {
+        console.log(`start of hook`);
+        console.log(srv.entities);
+        const { incidents } = srv.entities;
+        const query_get_ticketno = SELECT.one
+          .from(incidents)
+          .columns("max(ticket_no) as ticketno");
+        const result = await cds.run(query_get_ticketno);
+        const jsonobj = JSON.parse(JSON.stringify(result));
+        var ticketno = `${jsonobj.ticketno} + 1`;
+        req.data.ticket_no = JSON.stringify(eval(ticketno));
+        req.data.status = `PENDING`;
+        req.data.approverid = `K009287`;
+    })
+    this.on('CREATE', 'incident', async (req, msg) => {
+        createwf(msg.data);
+    });
 };
 
 // const createwf = async (incident) => {
